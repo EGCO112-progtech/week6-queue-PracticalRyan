f39c9e484352438b3060776c2b4e08b39dfc22c3
Ryan Gleeson <gleeson.ryanj@gmail.com>
2025-02-12 07:53:19 +0000

    first enqueue & dequeue

--- Node.h
+++ Node.h
@@ -9,35 +9,38 @@
 
 struct node
 {
-    int data;
-    struct node *nextPtr;
+  int data;
+  struct node *nextPtr;
 };
 
 typedef struct node Node;
-typedef struct node* NodePtr;
+typedef struct node *NodePtr;
 
-
-void enqueue(NodePtr * head, NodePtr* tail, int x){
-  Node* new_node=(NodePtr) malloc(sizeof(Node));
-if(new_node){ 
-    /* Finish queue*/
- }
+void enqueue(NodePtr *head, NodePtr *tail, int x)
+{
+  Node *new_node = (NodePtr)malloc(sizeof(Node));
+  if (new_node)
+  {
+    new_node->data = x;
+    new_node->nextPtr = NULL;
+    if (*head == NULL)
+      *head = new_node;
+    else
+      (*tail)->nextPtr = new_node;
+    *tail = new_node;
+  }
 }
 
-
-int dequeue(NodePtr* head, NodePtr* tail){
-  NodePtr t=*head;
-   if(t){
-   int value= t->data;
-   /* Finish dequeue*/
-       
-       
-   return value;
-   }
-   printf("Empty queue");
-   return 0;
+int dequeue(NodePtr *head, NodePtr *tail)
+{
+  NodePtr t = *head;
+  int value = t->data;
+  *head = t->nextPtr;
+  if (*head == NULL)
+    *tail = NULL;
+  free(t);
+  return value;
+  printf("Empty queue");
 }
-
-
 
 #endif
