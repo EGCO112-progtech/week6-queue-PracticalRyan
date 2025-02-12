0f5f9a9afbc038c915ffa00b5f8721903ecca412
Ryan Gleeson <gleeson.ryanj@gmail.com>
2025-02-12 08:19:10 +0000

    Updated print statement to match tests

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main(int argc, char **argv)
     if (strcmp(argv[i], "x") == 0)
     {
       x = dequeue_struct(&q);
-      printf("dequeueing %d\n", x);
+      printf("dequeing %d\n", x);
     }
     else
     {
